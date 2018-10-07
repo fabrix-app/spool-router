f0ca1fd608188e0af8988c6c21595d1d63df1cc6
scott-wyatt <scottwyatt86@gmail.com>
2018-10-07 11:18:53 -0400

    [chore] add archetype

---
+++ archetype/config/router.ts
@@ -0,0 +1,12 @@
+/**
+ * Router Configuration
+ *
+ */
+export const router = {
+  // Default Prefix to add to all routes where a prefix is not specified
+  prefix: null,
+  // Order in which the routes are listed (Some webservers require specific ordering)
+  sortOrder: 'asc',
+  // Debug router setup
+  debug: false
+}
